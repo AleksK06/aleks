169a324114c8d8aa61ef4e9b091ddb2c70756843
Aleksandr <aleksandr300906@gmail.com>
2025-10-02 00:14:02 +0300

    new commit

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> Моя страница </title>
+<link rel="stylesheet" href='mycss.css'>
+<link rel="shortcut icon" href='favicon.ico'>
+</head>
+<body>
+<div class="mydiv1">
 <?php
-	echo "Лабораторная работа №1 aak"
+	echo "Результат периметра прямоугольника со сторонами 10 и 12 равен ";
+	echo 10+10+12+12;
 ?>
+</div>
+</body>
+</html>
